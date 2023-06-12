09921df8f65d64effc07724f77bdf114006801ca
Kuba Tyszko <kuba@lbl.pl>
2023-06-12 10:28:49 -0700

    nico

---
+++ RaspberryPi_JetsonNano/python/examples/waveshare.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd2in13b_V3
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd2in13b_V3 Demo")
+    
+    epd = epd2in13b_V3.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear()
+    time.sleep(1)
+    
+    # Drawing on the image
+    logging.info("Drawing")    
+    font30 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 30)
+    font50 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 50)
+    
+    # Drawing on the Horizontal image
+    logging.info("1.Drawing on the Horizontal image...") 
+    HBlackimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126
+    HRYimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126  ryimage: red or yellow image  
+    drawblack = ImageDraw.Draw(HBlackimage)
+    drawry = ImageDraw.Draw(HRYimage)
+    drawblack.text((10, 20), "Nico's", font = font30, fill = 0)
+    drawblack.text((10, 50), "wallet", font = font30, fill = 0)
+    drawblack.text((100, 30), "$10", font = font50, fill = 0)
+    epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
+    time.sleep(2)
+    
+    logging.info("Goto Sleep...")
+    epd.sleep()
+        
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd2in13b_V3.epdconfig.module_exit()
+    exit()
+    
